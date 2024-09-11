5046745beb6f9c0a7eef921cbe2d720317b3d919
VICTOR ROSADO <VGROSADO04@GMAIL.COM>
2024-09-11 09:44:31 -0400

    function to calculate mortage created

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+function calculateMortgage(cost, term, interest, type) {
+
+    //convert percentage to decimal
+    let int = interest / 100 / 12;
+
+    //convert loan term from years to months
+    let months = term * 12;
+
+    let interestOnly = int * cost;
+
+    //caclulate montly payment
+    let monthlyPayment = cost * int * ((1 + int)**months) / (((1 + int)**months) - 1);
+
+    //format monthly payment to specified currency
+    const convertPayment = Intl.NumberFormat("en-US", {
+        style:"currency",
+        currency:"USD",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    })
+
+    //repayment object
+    let repayment = {
+        monthly: convertPayment.format(monthlyPayment),
+        total: convertPayment.format(monthlyPayment * months),
+        interest: convertPayment.format(interestOnly)
+    }
+
+    if (type === "interest-only") {
+        return console.log(repayment.interest);
+    } else { 
+        return console.log(repayment);
+    }
+
+}
+
+calculateMortgage(300000, 25, 5.25)
