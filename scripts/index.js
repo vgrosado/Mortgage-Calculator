0f3aa69062fb2612463dc65687cc55b80dc3db4e
VICTOR ROSADO <VGROSADO04@GMAIL.COM>
2024-09-15 20:43:35 -0400

    form validation written

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,45 @@
+const inputWrappers = document.querySelectorAll(".input__wrapper");
+const inp = document.getElementsByTagName("input");
+
+//convert input node arry into an iteratable array
+let inputs = Array.from(inp);
+console.log(inputs)
+//convert wrapper node arry into an iteratable array
+let wrappers = Array.from(inputWrappers);
+console.log(wrappers)
+
+//validation function that checks for input value and applies error styling to respective input and their wrappers.
+function validateForm(value, parent, label) {
+    let errorMsg = parent.querySelector(".input__error-msg");
+    if (!value.trim()) {
+        if (!errorMsg) {
+        errorMsg = document.createElement("p");
+        errorMsg.innerText = "This field is required";
+        errorMsg.classList.add("input__error-msg");
+        label.appendChild(errorMsg);
+        }
+        parent.classList.add("input__wrapper--error");
+    } else {
+        parent.classList.remove("input__wrapper--error");
+        if (errorMsg) {
+            label.removeChild(errorMsg);
+        }
+    }
+}
+
+// loop over each input and extract value, locate parent, label and pass down to validation function.
+inputs.forEach((input) => {
+    const parent = input.parentElement;
+    const label = parent.parentElement;
+    input.addEventListener("input", (event) => {
+        const val = event.target.value;
+        validateForm(val, parent, label);
+    })
+})
+
+
+
+
 
 function calculateMortgage(cost, term, interest, type) {
 
@@ -10,12 +52,12 @@ function calculateMortgage(cost, term, interest, type) {
     let interestOnly = int * cost;
 
     //caclulate montly payment
-    let monthlyPayment = cost * int * ((1 + int)**months) / (((1 + int)**months) - 1);
+    let monthlyPayment = cost * int * ((1 + int) ** months) / (((1 + int) ** months) - 1);
 
     //format monthly payment to specified currency
     const convertPayment = Intl.NumberFormat("en-US", {
-        style:"currency",
-        currency:"USD",
+        style: "currency",
+        currency: "USD",
         minimumFractionDigits: 0,
         maximumFractionDigits: 0,
     })
@@ -29,7 +71,7 @@ function calculateMortgage(cost, term, interest, type) {
 
     if (type === "interest-only") {
         return console.log(repayment.interest);
-    } else { 
+    } else {
         return console.log(repayment);
     }
 
